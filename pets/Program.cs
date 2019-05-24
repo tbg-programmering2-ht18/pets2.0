a6b093b1a11244d87a05206f4a88b608dfe3e44e
Axel Eriksson <Axel.DTK.Eriksson@lystkom.se>
2019-05-24 12:50:56 +0200

    Add project files.

---
+++ pets/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pets
+{
+    class Program
+    {
+        static List<Animal> animals = new List<Animal>();
+        private static readonly Random getrandom = new Random();
+
+        static void Main(string[] args)
+        {
+           
+            animals.Add(new Cat("Blixten"));
+            animals.Add(new Dog("Kliffe"));
+            animals.Add(new Bird("Pip"));
+
+            Console.WriteLine(@"This is a animal tester, see what spirit animal you have!
+            Press any key to see what you are! (esc. ends it all)");
+
+
+            // Every time a button get pushed except esc, it will continue shows random animals. 
+            // If esc is pressed, the program will exit.
+            ConsoleKey ck = Console.ReadKey().Key;
+            while (ck != ConsoleKey.Escape)
+            {
+                int x = getrandom.Next(0, animals.Count - 1);
+                Console.WriteLine("\nYour spirit animal is...");
+                Console.WriteLine(animals[x].Show());
+                Console.WriteLine("Press ESC to quit or any key to : \n");
+                ck = Console.ReadKey().Key;
+
+            }
+        }
+    }
+}
---
+++ pets/djur.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pets
+{
+    class Animal
+    {
+        public string Name { get; set; }
+        public string Sound { get; set; }
+        public bool CanFly { get; set; }
+
+        public string Show()
+        {
+            // Checks if the animal class  can fly, they all got a CanFly bool value.
+            // if the animal can't fly, returns can't fly.
+            if (CanFly == true)
+            {
+                return String.Format("name: {0}\nSound: {1}\nType: {2} and can fly", Name, Sound, GetType());
+            }
+            else
+            {
+                return String.Format("name: {0}\nSound: {1}\nType: {2}", Name, Sound, GetType());
+            }
+        }
+
+    }
+   
+    // that, in this case, are public and called in Program.cs code
+    class Dog : Animal
+    {
+        public Dog(String name)
+        {
+            Name = name;
+            Sound = "WOAF WOAF";
+            CanFly = false;
+        }
+    }
+    class Cat : Animal
+    {
+        public Cat(String name)
+        {
+            Name = name;
+            Sound = "MEOW";
+            CanFly = false;
+        }
+    }
+    class Bird : Animal
+    {
+        public Bird(String name)
+        {
+            Name = name;
+            Sound = "pipipi";
+            CanFly = true;
+        }
+    }
+}
